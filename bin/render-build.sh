d1ed7d3aac8bd20d6d293bf599299f03de26cf02
99p <poccori@gmail.com>
2023-01-08 14:06:09 +0900

    del: render.yaml, render-build.shを削除

--- bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle install
-yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile # cssはsprocketsを使っているため
-exec rails db:migrate
